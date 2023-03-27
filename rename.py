fbdd01b19a2d2d733400275035f97dd4688161fb
Yael Gras <grasyael@gmail.com>
2023-03-27 15:13:20 +0200

    Youpi

--- rename.py
+++ rename.py
@@ -13,12 +13,26 @@ for category in categories:
 
     images = os.listdir(images_path)
 
+
     enregistrees = 0
     for i in range(np.size(images)):
         path_image = os.path.join(images_path,images[i])
         extensions = images[i].split('.')
         extension = extensions[1]
-        if (not(category in images[i])):
-            os.rename(path_image,os.path.join(images_path , category + "_" + str(enregistrees) + "." + extension))
-            enregistrees += 1
+
+        enregistre = False
+        if category + "_" in path_image:
+            enregistre = True
+        
+        while not(enregistre):
+            if (not(category in images[i])):
+                try:
+                    os.rename(path_image,os.path.join(images_path , category + "_" + str(enregistrees) + "." + extension))
+                    enregistre = True
+                except FileExistsError:
+                    enregistre = False
+                finally : 
+                    enregistrees+=1
+  
+                
 
