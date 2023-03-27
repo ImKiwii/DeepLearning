8e48d42815f78cfd33d9e26927742805dec0ca94
entwan <antoine.girard.ptsi@gmail.com>
2023-03-27 10:51:25 +0200

    suppressions dossiers inutiles

---
+++ rename.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import os
+
+folder_path = "Images"
+categories = os.listdir(folder_path)
+print(categories)
+
+for category in categories:
+    images_path = os.path.join(folder_path, category, "triees")
+
+    images = os.listdir(images_path)
+
+    for i in range(np.length(images)):
+        print(images[i])
+        os.rename(images[i],path_dest+"/"+ str(passage) + "_"+imag)
